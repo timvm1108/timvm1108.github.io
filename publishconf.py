0168f948c780ecbd4118f481908bf680ae88eb14
Tim van Maren <timvanmaren@hotmail.com>
2021-02-08 17:36:11 -0700

    Changed URL

--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,11 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://timvm1108.github.io/'
+SITEURL = 'https://github.com/timvm1108/timvm1108.github.io/tree/src'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
